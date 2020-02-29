46b8a2a22d8f036e3a10cafeb3c01422e87db827
Felix Andreas <felix.andreas95@googlemail.com>
2020-03-01 00:15:46 +0100

    Python 3: Simple Tracking (#3)

--- examples/b2/simple_tracking/plot_simple_tracking.py
+++ examples/b2/simple_tracking/plot_simple_tracking.py
@@ -1,59 +1,28 @@
-#!/usr/bin/env python2
-import matplotlib
+import matplotlib.pyplot as plt
+from eleganttools import SDDS
 
-matplotlib.use("Agg")
-from pylab import *
-import matplotlib.patches as patches
+data = SDDS("output.w1").as_dict()
 
-from sddshzb import SDDSad
-import sys
+fig, axes = plt.subplots(3, 3, figsize=(12, 9))
 
-# Initialize SDDS object and load sdds file from a centroid watchpoint
-W = SDDSad(0)
-W.load("output.w1")
-# Get convenient attribute dictionary
-data = W.columnDataDict
-
-# Print out contained data
-print "\nDescription:", W.description
-print "\n", len(W.parameterName), "Paramters:\n", W.parameterName
-print "\n", len(W.columnName), "Columns:\n", W.columnName
-
-fig = figure(figsize=(12, 9))
-
-
-def eleplot(x, y, sel=None, label=None):
-    global data
-    print x, y
-    if sel is not None:
-        plot(data[x][sel], data[y][sel], ",", label=label)
-    else:
-        plot(data[x], data[y], ",", label=label)
-    xlabel(x)
-    ylabel(y)
+for ax, (x, y) in zip(
+    axes.flatten(),
+    [
+        ("Pass", "Cx"),
+        ("Pass", "Cxp"),
+        ("Cx", "Cxp"),
+        ("Pass", "Cy"),
+        ("Pass", "Cyp"),
+        ("Cy", "Cyp"),
+        ("Pass", "dCt"),
+        ("Pass", "Cdelta"),
+        ("dCt", "Cdelta"),
+    ],
+):
+    ax.plot(data[x], data[y], ",")
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
 
 
-subplot(331)
-eleplot("Pass", "dCt")
-subplot(334)
-eleplot("Pass", "Cdelta")
-subplot(337)
-eleplot("dCt", "Cdelta")
-
-subplot(332)
-eleplot("Pass", "Cx")
-subplot(335)
-eleplot("Pass", "Cxp")
-subplot(338)
-eleplot("Cx", "Cxp")
-
-subplot(333)
-eleplot("Pass", "Cy")
-subplot(336)
-eleplot("Pass", "Cyp")
-subplot(339)
-eleplot("Cy", "Cyp")
-
-
-tight_layout()
-savefig("tracking.pdf")
+plt.tight_layout()
+plt.savefig("tracking.pdf")
