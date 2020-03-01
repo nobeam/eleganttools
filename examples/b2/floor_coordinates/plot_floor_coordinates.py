c5111c6d92a97fecb47da4b4b7360f75fd7980cf
Felix Andreas <felix.andreas95@googlemail.com>
2020-03-01 01:23:58 +0100

    push all

--- examples/b2/floor_coordinates/plot_floor_coordinates.py
+++ examples/b2/floor_coordinates/plot_floor_coordinates.py
@@ -7,7 +7,7 @@ from eleganttools import SDDS, draw_lattice
 data = SDDS("data.flr").as_dict()
 
 plt.figure(figsize=(8, 8))
-plt.plot(data["Z"], data["X"], "-")
+plt.plot(data["Z"], data["X"], "-")  # TODO: draw magnets
 plt.xlim(-40, 40)
 plt.ylim(-78, 2)
 plt.grid()
